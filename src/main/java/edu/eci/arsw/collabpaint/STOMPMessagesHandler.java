6c89e586d7e8c030c1c1e5a412e0324fd8d3dc55
d.castiblanco <dolardanies@hotmail.com>
2018-11-22 13:35:51 -0500

    Final

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -11,16 +11,31 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class STOMPMessagesHandler {
-	
-	@Autowired
-	SimpMessagingTemplate msgt;
-    
-	@MessageMapping("/newpoint.{numdibujo}")    
-	public void handlePointEvent(Point pt,@DestinationVariable String numdibujo) throws Exception {
-		System.out.println("Nuevo punto recibido en el servidor!:"+pt);
-		msgt.convertAndSend("/topic/newpoint"+numdibujo, pt);
-	}
-} 
+
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    ConcurrentHashMap<String, ArrayList<Point>> polygonPoints = new ConcurrentHashMap();
+
+    @MessageMapping("/newpoint.{numdibujo}")
+    public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
+        System.out.println("Nuevo punto recibido en el servidor!:" + pt);
+        if (polygonPoints.containsKey(numdibujo)) {
+            polygonPoints.get(numdibujo).add(pt);
+            if (polygonPoints.get(numdibujo).size() > 3) {
+                msgt.convertAndSend("/topic/newpolygon." + numdibujo, polygonPoints.get(numdibujo));
+                polygonPoints.clear();
+            }
+        } else {
+            ArrayList<Point> puntos = new ArrayList();
+            puntos.add(pt);
+            polygonPoints.put(numdibujo, puntos);
+        }
+        //msgt.convertAndSend("/topic/newpoint." + numdibujo, pt);
+    }
+}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,12 +9,25 @@ var app = (function () {
 
     var stompClient = null;
     var topic;
+    var canvas;
 
     var addPointToCanvas = function (point) {
-        var canvas = document.getElementById("canvas");
+
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+    };
+
+    var addPolygonToCanvas = function (points) {
+        var ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        for (let i = 0; i < points.length - 1; i++) {
+            ctx.moveTo(points[i].x, points[i].y);
+            ctx.lineTo(points[i + 1].x, points[i + 1].y);
+        }
+        ctx.moveTo(points[points.length - 1].x, points[points.length - 1].y);
+        ctx.lineTo(points[0].x, points[0].y);
         ctx.stroke();
     };
 
@@ -39,47 +52,50 @@ var app = (function () {
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/topic/newpoint.' + topic, function (eventBody) {
-                var point=JSON.parse(eventBody.body);              
-                addPointToCanvas(point);
-            
+             var point=JSON.parse(eventBody.body);              
+             addPointToCanvas(point);
+             
+             });
+            stompClient.subscribe('/topic/newpolygon.' + topic, function (eventBody) {
+                var points = JSON.parse(eventBody.body);
+                addPolygonToCanvas(points);
             });
         });
 
     };
-    
-    var eventHandler = function(evt) {
+
+    var eventHandler = function (evt) {
         let pos = getMousePosition(evt);
-        app.publishPoint(pos.x, pos.y)
+        stompClient.send("/app/newpoint." + topic, {}, JSON.stringify(new Point(pos.x, pos.y))); 
         /*var pt = new Point(pos.x, pos.y);
-        stompClient.send("/topic/newpoint." + topic, {}, JSON.stringify(pt)); 
-        stompClient.send("/app/newpoint." + topic, {}, JSON.stringify(pt));*/ 
+         stompClient.send("/topic/newpoint." + topic, {}, JSON.stringify(pt)); 
+         stompClient.send("/app/newpoint." + topic, {}, JSON.stringify(pt));*/
     };
 
 
     return {
 
         init: function () {
-            var can = document.getElementById("canvas");
+            canvas = document.getElementById("canvas");
             //websocket connection
             if (window.PointerEvent) {
-                can.addEventListener("pointerdown", eventHandler);
+                canvas.addEventListener("pointerdown", eventHandler);
             } else {
-                can.addEventListener("mousedown", eventHandler);
-            }                
+                canvas.addEventListener("mousedown", eventHandler);
+            }
         },
 
         publishPoint: function (px, py) {
             var pt = new Point(px, py);
             console.info("publishing point at " + pt);
-            addPointToCanvas(pt);
-			
+            
+
             console.log(JSON.stringify(pt));
             //publicar el evento
             stompClient.send("/topic/newpoint." + topic, {}, JSON.stringify(pt));
-            stompClient.send("/app/newpoint." + topic, {}, JSON.stringify(pt));
         },
-        
-        subscribeTo: function(topicToSubscribe){
+
+        subscribeTo: function (topicToSubscribe) {
             topic = topicToSubscribe;
             connectAndSubscribe();
         },
